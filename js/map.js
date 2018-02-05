b7ccd12226776d62319bd87f3ed8bb20715e7a50
LINSquad <lina.vishnyakova@outlook.com>
2018-02-05 19:43:42 +0400

    Работающий вариант)

--- js/map.js
+++ js/map.js
@@ -77,10 +77,13 @@ var randomLenght = function (mass) {
 };
 
 var randomNumber = function (min, max) {
-   return Math.round(Math.random() * (max - min) + min);
+  return Math.round(Math.random() * (max - min) + min);
 };
 
+
 var generateAdData = function() {
+  var locatX = randomNumber(900, 300);
+  var locatY = randomNumber(500, 150);
   return {
     author: {
       avatar: 'img/avatars/user0' + randomNumber(8, 1) + '.png'
@@ -88,7 +91,7 @@ var generateAdData = function() {
 
     offer: {
       title: randomElement(titlesCopy),
-      address: location.x + ', ' + location.y,
+      address: locatX + ', ' + locatY,
       price: randomNumber(1000000, 1000),
       type: randomElement(typesCopy),
       rooms: randomNumber(5, 1),
@@ -100,9 +103,9 @@ var generateAdData = function() {
       photos: randomMass(photosCopy)
     },
 
-    location: {
-      x: randomNumber(900, 300),
-      y: randomNumber(500, 150)
+    locat: {
+      x: locatX,
+      y: locatY
     }
   }
 };
@@ -118,19 +121,21 @@ var fragment = document.createDocumentFragment();
 
 for (i = 0; i < 8; i++) {
   var newElement = document.createElement('button');
-  newElement.style = 'left: ' + (nearByAds[i].location.x + 20) + 'px; top: ' + (nearByAds[i].location.y + 40) + 'px;';
+  newElement.style = 'left: ' + (nearByAds[i].locat.x + 20) + 'px; top: ' + (nearByAds[i].locat.y + 40) + 'px;';
   newElement.className = 'map__pin';
 
-  var newElementImg = document.createElement('img');
-  newElementImg.src = nearByAds[i].author.avatar;
-  newElementImg.width = '40';
-  newElementImg.height = '40';
-  newElementImg.setAttribute('draggable', 'false');
+  var newImg = document.createElement('img');
+  newImg.src = nearByAds[i].author.avatar;
+  newImg.width = '40';
+  newImg.height = '40';
+  newImg.setAttribute('draggable', 'false');
 
-  newElement.appendChild(newElementImg);
+  newElement.appendChild(newImg);
   fragment.appendChild(newElement);
 }
 mapPins.appendChild(fragment);
+
+document.querySelector('.map').classList.remove('map--faded');
 
 var similarTemplate = document.querySelector('template').content.querySelector('article');
 
@@ -141,7 +146,7 @@ var renderPosts = function () {
   newPosts.querySelector('h3').innerText = nearByAds[i].offer.title;
   newPosts.querySelector('small').innerText = nearByAds[i].offer.address;
   console.log(nearByAds[i]);
-  newPosts.querySelector('.popup__price').innerText = nearByAds[i].offer.price + ' &#x20bd;/ночь';
+  newPosts.querySelector('.popup__price').innerText = nearByAds[i].offer.price + ' ₽/ночь';
   if (nearByAds[i].offer.type === 'flat') {
     newPosts.querySelector('h4').innerText = 'Квартира';
   } else if (nearByAds[i].offer.type === 'bungalo') {
@@ -151,12 +156,38 @@ var renderPosts = function () {
   }
   massP[2].innerText =  nearByAds[i].offer.rooms + ' комнаты для ' + nearByAds[i].offer.guests + ' гостей';
   massP[3].innerText = 'Заезд после ' + nearByAds[i].offer.checkin + ', выезд до ' + nearByAds[i].offer.checkout;
+  newPosts.querySelector('.popup__features').style.paddingLeft = '0';
+
   var featureAll = newPosts.querySelectorAll('ul.popup__features > li');
-  for (var j = 0; j < nearByAds[i].offer.features.length; j++) {
-    featureAll[j].className = 'feature feature--' + nearByAds[i].offer.features[j];
+  console.log(nearByAds[i].offer.features.length);
+  for (var j = 0; j < featureAll.length; j++) {
+    console.log(nearByAds[i].offer.features[j]);
+    if(nearByAds[i].offer.features[j] === undefined) {
+      featureAll[j].style.display = 'none';
+    } else {
+      featureAll[j].className = 'feature feature--' + nearByAds[i].offer.features[j];
+    }
+
   }
   massP[4].innerText = nearByAds[i].offer.description;
-  newPosts.querySelector('li > img').src = nearByAds[i].offer.photos;
+
+  var ulPictures = newPosts.querySelector('.popup__pictures');
+  ulPictures.style.paddingLeft = '0';
+  for (j = 0; j < 3; j++) {
+    var newElement = document.createElement('li');
+
+    var newElementImg = document.createElement('img');
+    newElementImg.src = nearByAds[i].offer.photos[j];
+    newElementImg.width = '60';
+    newElementImg.height = '60';
+    newElementImg.style.paddingRight = '3px';
+
+    newElement.appendChild(newElementImg);
+    fragment.appendChild(newElement);
+  }
+  ulPictures.appendChild(fragment);
+
+  newPosts.querySelector('.popup__avatar').src = nearByAds[i].author.avatar;
 
   return newPosts;
 };
@@ -165,8 +196,4 @@ for (i = 0; i < 1; i++) {
 }
 var foo = document.querySelector('.map');
 foo.appendChild(fragment);
-console.log(foo);
-
-var mapFiltersContainer = document.querySelector('.map__filters-container')
-mapFiltersContainer.insertAdjacentHTML('beforeend', similarTemplate.appendChild(fragment));
 
