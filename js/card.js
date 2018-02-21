81dd9503e8b1708af10b3e56c1ee64a1f21210e0
LINSquad <lina.vishnyakova@outlook.com>
2018-02-21 20:41:39 +0400

    Деление на модули

---
+++ js/card.js
@@ -0,0 +1,72 @@
+'use strict';
+(function () {
+  var similarTemplate = document.querySelector('template').content.querySelector('article');
+  var fragment = document.createDocumentFragment();
+
+  window.card = function (element) {
+    var newPosts = similarTemplate.cloneNode(true);
+    var massP = newPosts.querySelectorAll('p');
+    var elementInnerText = function (element, str) {
+      newPosts.querySelector(element).innerText = str;
+    };
+    newPosts.className = 'map__card';
+    elementInnerText('h3', element.offer.title);
+    elementInnerText('small', element.offer.address);
+    elementInnerText('.popup__price', element.offer.price.toLocaleString() + ' ₽/ночь');
+
+    if (element.offer.type === 'flat') {
+      elementInnerText('h4', 'Квартира');
+    } else if (element.offer.type === 'bungalo') {
+      elementInnerText('h4', 'Бунгало');
+    } else {
+      elementInnerText('h4', 'Дом');
+    }
+
+    if (element.offer.rooms === 1) {
+      massP[2].innerText = element.offer.rooms + ' комната для ' + element.offer.guests + ' гостей';
+    } else if (element.offer.rooms === 5) {
+      massP[2].innerText = element.offer.rooms + ' комнат для ' + element.offer.guests + ' гостей';
+    } else {
+      massP[2].innerText = element.offer.rooms + ' комнаты для ' + element.offer.guests + ' гостей';
+    }
+
+    massP[3].innerText = 'Заезд после ' + element.offer.checkin + ', выезд до ' + element.offer.checkout;
+    newPosts.querySelector('.popup__features').style.paddingLeft = '0';
+
+    var featureAll = newPosts.querySelectorAll('ul.popup__features > li');
+    for (j = 0; j < featureAll.length; j++) {
+      featureAll[j].style.display = 'none';
+    }
+    for (var j = 0; j < element.offer.features.length; j++) {
+      featureAll[j].style.display = '';
+      featureAll[j].className = 'feature feature--' + element.offer.features[j];
+    }
+
+    massP[4].innerText = element.offer.description;
+
+    var ulPictures = newPosts.querySelector('.popup__pictures');
+    ulPictures.style.paddingLeft = '0';
+
+    var createImg = function () {
+      var newElement = document.createElement('li');
+
+      var newElementImg = document.createElement('img');
+      newElementImg.src = element.offer.photos[j];
+      newElementImg.width = '60';
+      newElementImg.height = '60';
+      newElementImg.style.paddingRight = '3px';
+
+      newElement.appendChild(newElementImg);
+      return newElement;
+    };
+
+    for (j = 0; j < 3; j++) {
+      fragment.appendChild(createImg());
+    }
+    ulPictures.appendChild(fragment);
+
+    newPosts.querySelector('.popup__avatar').src = element.author.avatar;
+
+    return newPosts;
+  }
+})();
